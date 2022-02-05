bc469c3b09ac3fa1018da138691a9d3561f35a67
Heber Baraybar <heber.bar@gmail.com>
2022-02-05 15:42:22 +0100

    Added-button-ack-forward

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,44 @@
 async function app() {
-  fetch('https://pokeapi.co/api/v2/pokemon/')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  let index = 0;
+  let URL = `https://pokeapi.co/api/v2/pokemon/?offset=${index}&limit=25`;
+  let data = await pokemonResult(URL);
+
+  console.log(data);
+  await pokemonList(data);
+
+  async function pokemonResult() {
+    const response = await fetch(URL);
+    return response.json;
+  }
+
+  async function pokemonList(data) {
+    let template = '';
+    data.results.forEach((item) => {
+      template += `
+      <li><a href="${item.url}"> ${item.name}</a></li>`;
+    });
+    document.querySelector('.pokemon-list').innerHTML = template;
+  }
+
+  async function buttonBack() {
+    console.log('Index = ' + index);
+    index -= 25;
+    URL = `https://pokeapi.co/api/v2/pokemon/?offset=${index}&limit=25`;
+    let data = await pokemonResult(URL);
+    pokemonList(data);
+  }
+
+  async function buttonNext() {
+    console.log('Index = ' + index);
+    index += 25;
+    URL = `https://pokeapi.co/api/v2/pokemon/?offset=${index}&limit=25`;
+    let data = await pokemonResult(URL);
+    console.log(data);
+    pokemonList(data);
+  }
+
+  document
+    .querySelector('#button-previous')
+    .addEventListener('click', buttonBack);
+  document.querySelector('#button-next').addEventListener('click', buttonNext);
 }
-document.addEventListener('DOMContentLoaded', app);
